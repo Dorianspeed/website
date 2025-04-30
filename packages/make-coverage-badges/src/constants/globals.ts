7a1f2eb478e22e58803d1ff5f4e757022002d4f7
Dorian Garcia <dorian.garcia.dev@gmail.com>
2025-04-30 18:52:07 +0200

    refactor: use markdown comments instead of sections to handle badges creation

--- packages/make-coverage-badges/src/constants/globals.ts
+++ packages/make-coverage-badges/src/constants/globals.ts
@@ -1,9 +1,9 @@
+export const COVERAGES_SECTION_START = '<!-- COVERAGES:START -->';
+export const COVERAGES_SECTION_END = '<!-- COVERAGES:END -->';
 export const DEFAULT_BREAK_LINE = '\n\n';
 export const DEFAULT_PROJECTS_DIRECTORY = '../../coverage';
 export const JSON_COVERAGE_FILE = 'coverage-summary.json';
-export const NEXT_SECTION = '## Other scripts';
 export const README_PATH = '../../README.md';
-export const TEST_COVERAGE_SECTION = '## Tests coverage';
 
 export const PROJECTS_LABELS = {
   'make-coverage-badges': '@website/make-coverage-badges',
