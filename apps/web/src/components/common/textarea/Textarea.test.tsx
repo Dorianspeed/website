41642a1a21362abeff03edc4428bb6a31e308fe3
Dorian Garcia <dorian.garcia.dev@gmail.com>
2025-05-18 15:42:58 +0200

    feat: add textarea counter

--- apps/web/src/components/common/textarea/Textarea.test.tsx
+++ apps/web/src/components/common/textarea/Textarea.test.tsx
@@ -29,6 +29,26 @@ describe('Textarea', () => {
 
     expect(screen.getByLabelText('Message')).toBeInTheDocument();
     expect(screen.getByRole('textbox', { name: 'Message' })).toBeInTheDocument();
+    expect(screen.getByTestId('charCounter')).toHaveTextContent('500 caractères restants sur 500');
+    expect(screen.getByTestId('sr-charCounter')).toHaveTextContent(
+      '500 caractères restants sur 500'
+    );
     expect(screen.getByTestId('textarea-error')).toHaveClass('visible');
+  });
+
+  it('should ajust characters count', () => {
+    vi.spyOn(reactHookForm, 'useController').mockReturnValue({
+      field: {
+        name: 'message',
+        value:
+          '1O6L7h6XeV55X70cEtLrLh37e4yZBxNhaOq5DTqy27dNtpuN2nu1Cofl4FPpqcaKD3irVAnloRCDu8zRwvRHxWhZZmiEUgsL9nkf8z7S5id2riKnx8Ex7Pl3PFBbE9aDyZwVfWVcNEZL4GbEAgy7ixI9ZWG48N3mPm6nPM9exIkU4CSk6Kzdl5rS0mLCK94fcjclC6THJTWVCz9gGxu23FJSWKJ8OlZZ6iRMxTH2p90B5ISMw9PzIHZ0IY9ZYTuCItQVezuCkp0pPB7qlanGeHfNURLst6VkxObrFCU1TuRYQwRnEyzqp8xk29BirpNVX1OcKgLddj3hPtR4xSGmAxfSL0K2qOYF5NTg2sIeU1cchMfF2ATOPGnbe7cUSazYWBnjwVi7DUyZ2KS8EunfWjVy8bHKHZ6mfKlynsDElhEiStROPn82WengaOp67Bh7c8QKVpeR3vw04Uv5dZo5NBM9l7vxQzCb63kJXnqhjLq9CrfyiI6X2324'
+      } as unknown as reactHookForm.ControllerRenderProps,
+      fieldState: {} as unknown as reactHookForm.ControllerFieldState,
+      formState: {} as unknown as reactHookForm.UseFormStateReturn<ContactFormProps>
+    });
+
+    render(<Textarea {...defaultTextareaProps} control={control} />);
+
+    expect(screen.getByTestId('charCounter')).toHaveTextContent('0 caractère restant sur 500');
   });
 });
--- apps/web/src/components/contact/contactForm/ContactForm.schema.tsx
+++ apps/web/src/components/contact/contactForm/ContactForm.schema.tsx
@@ -1,19 +1,25 @@
 import zod from 'zod';
 
-const MIN_CHAR_MESSAGE = 'Minimum 2 caractères';
-const MAX_CHAR_MESSAGE = 'Maximum 50 caractères';
+const MAX_CHAR_LENGTH = 50;
+const MAX_CHAR_MESSAGE = `Maximum ${MAX_CHAR_LENGTH} caractères`;
+const MIN_CHAR_LENGTH = 2;
+const MIN_CHAR_MESSAGE = `Minimum ${MIN_CHAR_LENGTH} caractères`;
+export const TEXTAREA_MAX_LENGTH = 500;
 
 export const contactFormSchema = zod.object({
   companyName: zod
     .string()
-    .min(2, { message: MIN_CHAR_MESSAGE })
-    .max(50, { message: MAX_CHAR_MESSAGE }),
+    .min(MIN_CHAR_LENGTH, { message: MIN_CHAR_MESSAGE })
+    .max(MAX_CHAR_LENGTH, { message: MAX_CHAR_MESSAGE }),
   email: zod.string().email({ message: 'Le format est incorrect' }),
   message: zod
     .string()
-    .min(2, { message: MIN_CHAR_MESSAGE })
-    .max(500, { message: 'Maximum 500 caractères' }),
-  name: zod.string().min(2, { message: MIN_CHAR_MESSAGE }).max(50, { message: MAX_CHAR_MESSAGE })
+    .min(MIN_CHAR_LENGTH, { message: MIN_CHAR_MESSAGE })
+    .max(TEXTAREA_MAX_LENGTH, { message: `Maximum ${TEXTAREA_MAX_LENGTH} caractères` }),
+  name: zod
+    .string()
+    .min(MIN_CHAR_LENGTH, { message: MIN_CHAR_MESSAGE })
+    .max(MAX_CHAR_LENGTH, { message: MAX_CHAR_MESSAGE })
 });
 
 export type ContactFormProps = zod.infer<typeof contactFormSchema>;
